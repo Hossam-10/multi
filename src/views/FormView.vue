870cb41e954a3084ebfebef0a0f9888c2feb806d
Hossam-M10 <148749322+Hossam-M10@users.noreply.github.com>
2023-10-30 12:40:57 +0200

    Fix bacground size

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -2,10 +2,10 @@
   <div class="d-flex align-center h-100" v-if="!xs">
     <v-card height="580" width="60%" class="pa-2 mx-auto">
       <v-row>
-        <v-col cols="4">
+        <v-col cols="3">
           <FormSidebar />
         </v-col>
-        <v-col cols="8">
+        <v-col cols="9">
           <Form />
         </v-col>
       </v-row>
